f385d1809f987bbb51fc70e5b3b7d39acacf356e
Stephan <stephan.startsev@gmail.com>
2019-12-02 11:28:12 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './app.scss'
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.scss'
+import App from './components/App/App'
+
+ReactDOM.render(<App/> ,document.getElementById('app'))
